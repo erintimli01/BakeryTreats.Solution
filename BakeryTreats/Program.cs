9bcf5e224d3ff0e1ae32259d2da991987195f40f
Erin Timlin <erintimlin@gmail.com>
2023-03-24 13:10:59 -0400

    add password settings

--- BakeryTreats/Program.cs
+++ BakeryTreats/Program.cs
@@ -26,6 +26,16 @@ namespace BakeryTreats
                 .AddEntityFrameworkStores<BakeryTreatsContext>()
                 .AddDefaultTokenProviders();
 
+      builder.Services.Configure<IdentityOptions>(options =>
+{
+  options.Password.RequireDigit = true;
+  options.Password.RequireLowercase = true;
+  options.Password.RequireNonAlphanumeric = true;
+  options.Password.RequireUppercase = true;
+  options.Password.RequiredLength = 6;
+  options.Password.RequiredUniqueChars = 1;
+});
+
       WebApplication app = builder.Build();
 
       // app.UseDeveloperExceptionPage();
